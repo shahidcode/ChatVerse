b43d57eabaed40af1c61f6ae2c1199c79fecb4b5
Shahid Nadaf <6.shahidnadaf@gmail.com>
2023-08-11 16:04:54 +0530

    Update people.js

--- server/routes/people.js
+++ server/routes/people.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { people } from '../controllers/people.js';
 const router = express.Router();
 
-router.get('/',people)    // :/people/
+router.get('/',people)   
 
 export default router;
